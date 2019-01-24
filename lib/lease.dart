ca4c67fd400870115c6bfd03a1aaf25c42e6f022
Sushruth P <44200208+sushruth-06@users.noreply.github.com>
2019-01-24 11:52:59 +0530

    Update lease.dart

--- lib/lease.dart
+++ lib/lease.dart
@@ -2,8 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:cached_network_image/cached_network_image.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 class Lease extends StatelessWidget{
+
+  final a=TextEditingController();
+  final b=TextEditingController();
+  final c=TextEditingController();
+  final d=TextEditingController();
+  final e=TextEditingController();
+
+  final CollectionReference accData=
+      Firestore.instance.collection('rent_vehicles');
+  static final formkey=GlobalKey<FormState>();
+
+  submitAcc(String name,String location,String cost,String contact,String owner){
+    Map<String,String> data=<String,String>{
+      "contact":"$contact",
+      "cost":"$cost",
+      "location":"$location",
+      "name":"$name",
+      "owner":"$owner"
+    };
+    accData.add(data);
+  }
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -37,6 +58,7 @@ class Lease extends StatelessWidget{
                     Padding(
                       padding: const EdgeInsets.all(10.0),
                       child: TextFormField(
+                        controller: a,
                         keyboardType: TextInputType.text,
                         //controller: name,
                         decoration: InputDecoration(
@@ -56,6 +78,7 @@ class Lease extends StatelessWidget{
                     Padding(
                       padding: const EdgeInsets.all(10.0),
                       child: TextFormField(
+                        controller: b,
                         keyboardType: TextInputType.text,
                         //controller: name,
                         decoration: InputDecoration(
@@ -74,6 +97,26 @@ class Lease extends StatelessWidget{
                     Padding(
                       padding: const EdgeInsets.all(10.0),
                       child: TextFormField(
+                        controller: c,
+                        keyboardType: TextInputType.text,
+                        //controller: name,
+                        decoration: InputDecoration(
+                            border: new OutlineInputBorder(
+                              borderRadius: new BorderRadius.circular(25.0),
+                              borderSide: new BorderSide(),
+                            )),
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Enter the name';
+                          }
+                        },
+                      ),
+                    ),
+                    Text('Owner Name:',style: TextStyle(fontSize: 25,fontFamily: "Montserrat")),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: TextFormField(
+                        controller: e,
                         keyboardType: TextInputType.text,
                         //controller: name,
                         decoration: InputDecoration(
@@ -92,6 +135,7 @@ class Lease extends StatelessWidget{
                     Padding(
                       padding: const EdgeInsets.all(10.0),
                       child: TextFormField(
+                        controller: d,
                         keyboardType: TextInputType.text,
                         //controller: name,
                         decoration: InputDecoration(
@@ -109,7 +153,11 @@ class Lease extends StatelessWidget{
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: <Widget>[
-                        RaisedButton(onPressed:null,color: Colors.blueAccent,
+                        RaisedButton(onPressed:()
+                        {
+                          submitAcc(a.text,b.text,c.text,d.text,e.text);
+
+                        },color: Colors.blueAccent,
                           child: Text('SUBMIT',style: TextStyle(fontSize: 20,color: Colors.white,fontFamily: "Montserrat"),),
                         ),
                         RaisedButton(onPressed:null,color: Colors.white,
